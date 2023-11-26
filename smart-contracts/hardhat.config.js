76f1a45b53e7dad36cbd02b04f03d9930de52898
Eloh <elowine.tauro@ecocashholdings.co.zw>
2023-11-26 12:41:33 +0200

    Deployed STKN Contract

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,4 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -6,7 +10,29 @@ module.exports = {
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545/",
+      accounts: [PRIVATE_KEY],
       chainId: 31337,
     },
+
+    alfajores: {
+      url: "https://alfajores-forno.celo-testnet.org",
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      chainId: 44787
+    },
+
+    celo: {
+      url: "https://forno.celo.org",
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      chainId: 42220
+    }
   },
+  namedAccounts: {
+    deployer: {
+        default: 0, // here this will by default take the first account as deployer
+        1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+    player: {
+        default: 1,
+    },
+},
 };
--- smart-contracts/scripts/deploy.js
+++ smart-contracts/scripts/deploy.js
@@ -3,20 +3,22 @@ const { ethers } = require("hardhat");
 async function main() {
   //STKN
   console.log("Deploying STKN Contract...");
-  const STKNFactory = await ethers.getContractFactory("STKN");
-  const stkn = await STKNFactory.deploy();
+  const stkn = await ethers.deployContract("STKN");
+  await stkn.waitForDeployment();
 
-  console.log("Deployed STKN:", stkn.address);
+  console.log("Deployed STKN:", stkn.target);
 
   //STKNICO
   console.log("Deploying stknICO Contract...");
   const StknICOFactory = await ethers.getContractFactory("StknICO");
   const stknICO = await StknICOFactory.deploy(
     "0x00f2a05f8327ac26e1994b92dbd4e4813bfa8609",
-    stkn.address
+    stkn.target
   );
 
-  console.log("Deployed stknICO:", stknICO.address);
+  await stknICO.waitForDeployment();
+
+  console.log("Deployed stknICO:", stknICO.target);
 }
 
 main()
